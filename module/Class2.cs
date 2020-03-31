298215c5c758379a3a669abdff3fdf80e877d0db
VadymDurkot <57210295+VadymDurkot@users.noreply.github.com>
2020-03-31 15:00:58 +0300

    Add files via upload

---
+++ module/Class2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Square s = new Square(3);
+            Square s2 = new Square(2);
+
+            Console.WriteLine(s);
+            Console.WriteLine(s + s2);
+            Console.WriteLine(s - s2);
+            Console.WriteLine(s * 3);
+            Console.WriteLine(s.Area());
+            Console.WriteLine(s.Perimeter());
+        }
+    }
+}
---
+++ module/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module
+{
+    public class Square
+    {
+        private double side;
+        private const double comparsion_eps = 0.00001;
+        public double Side
+        {
+            get
+            {
+                return side;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("square side must be non-negative");
+                }
+                side = value;
+            }
+        }
+        public Square(double side)
+        {
+            Side = side;
+        }
+        public Square() : this(0)
+        {
+
+        }
+        public Square(Square other)
+        {
+            Side = other.Side;
+        }
+        public void Input()
+        {
+            Console.Write("Square Side =");
+            Side = Convert.ToDouble(Console.ReadLine());
+        }
+        public override string ToString()
+        {
+            return $"Square({Side})";
+        }
+        public double Area()
+        {
+            return Side * Side;
+        }
+        public double Perimeter()
+        {
+            return Side * 4;
+        }
+        public static bool operator ==(Square s1, Square s2)
+        {
+            return Math.Abs(s1.Side - s2.Side) <= comparsion_eps;
+        }
+        public static bool operator !=(Square s1, Square s2)
+        {
+            return !(s1 == s2);
+        }
+        public static Square operator +(Square s1, Square s2)
+        {
+            return new Square(s1.Side + s2.Side);
+        }
+        public static Square operator -(Square s1, Square s2)
+        {
+            return new Square(s1.Side - s2.Side);
+        }
+        public static Square operator *(Square sq, double scalar)
+        {
+            return new Square(sq.Side * scalar);
+        }
+    }
+}
+    
+
